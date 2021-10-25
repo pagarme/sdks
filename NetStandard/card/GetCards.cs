2b60cabd0af2a2be86c3c1c8f840b9f7be4ed902
Magno Moreira <magno.moreira@pagar.me>
2021-10-25 09:07:59 -0300

    Example NetStandard

---
+++ NetStandard/card/GetCards.cs
@@ -0,0 +1,25 @@
+﻿using PagarmeCoreApi.Standard;
+using System;
+
+namespace Examples.card
+{
+    class GetCards
+    {
+        static void Main(string[] args)
+        {
+
+            // The username to use with basic authentication
+            string basicAuthUserName = "sk_test_4tdVXpseumRmqbo";
+            // The password to use with basic authentication
+            string basicAuthPassword = "";
+
+            var client = new PagarmeCoreApiClient(basicAuthUserName, basicAuthPassword);
+
+            string customerId = "cus_6l5dMWZ0hkHZ4XnE";
+            string cardId = "card_8ELY0AwVF9HDa3jK";
+
+            var response = client.Customers.GetCard(customerId, cardId);
+
+        }
+    }
+}
---
+++ NetStandard/charge/CreateCaptureChargePartial.cs
@@ -0,0 +1,27 @@
+﻿using PagarmeCoreApi.Standard;
+using PagarmeCoreApi.Standard.Models;
+
+namespace Example.Charges
+{
+    class CreateCaptureChargePartial
+    {
+        static void Main(string[] args)
+        {
+            string basicAuthUserName = "sk_test_4tdVXpseumRmqbo"; // The username to use with basic authentication
+            string basicAuthPassword = ""; // The password to use with basic authentication
+
+            var client = new PagarmeCoreApiClient(basicAuthUserName, basicAuthPassword);
+            string chargeId = "ch_exRAY21fvNFVD9EX";
+
+            var request = new CreateCaptureChargeRequest
+            {
+                Amount = 100,
+                Code = "capture_partial_operation"
+            };
+
+            client.Charges.CaptureChargeAsync(chargeId, request);
+
+        }
+
+    }
+}
