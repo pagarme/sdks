a1d2ed788ea6dc3df7856022328d2a91e9099412
Magno <magno.moreira@pagar.me>
2021-11-05 16:04:43 -0300

    Example netstandard (#11)
    * Example NetStandard
    
    * Sk_test removal
    
    * Refactoring

---
+++ NetStandard/charge/CreateCaptureChargePartial.cs
@@ -0,0 +1,29 @@
+﻿using PagarmeCoreApi.Standard;
+using PagarmeCoreApi.Standard.Models;
+
+namespace Example.Charges
+{
+    class CreateCaptureChargePartial
+    {
+        static void Main(string[] args)
+        {
+            // Configuration parameters and credentials
+            string basicAuthUserName = "basicAuthUserName"; // The username to use with basic authentication
+            string basicAuthPassword = "basicAuthPassword"; // The password to use with basic authentication
+
+            var client = new PagarmeCoreApiClient(basicAuthUserName, basicAuthPassword);
+            
+            string chargeId = "ch_exRAY21fvNFVD9EX";
+
+            var request = new CreateCaptureChargeRequest
+            {
+                Amount = 100,
+                Code = "capture_partial_operation"
+            };
+
+            client.Charges.CaptureChargeAsync(chargeId, request);
+
+        }
+
+    }
+}
